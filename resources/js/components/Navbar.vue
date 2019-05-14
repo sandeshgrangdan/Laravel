bc1f758064dd5792f6c4dadfa739525a99f28817
sandeshgrangdan <sandesht801@gmail.com>
2019-05-14 21:07:08 +0545

    final

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+	 <div class="navbar navbar-expand-sm navbar-dark bg-info mb-2">
+	 	<div class="container">
+		<a href="#" class="navbar-brand"> Larticles </a>
+		</div>
+	 </div>
+</template>  
